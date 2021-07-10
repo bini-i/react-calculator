d9d4f3c1e9fb729373ac37d4df0a4973c9ee9d27
bini-i <binyam.sh1@gmail.com>
2021-07-10 11:20:42 +0300

    Update Button.js
    - add name props
    - add proptypes validation

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Button(props) {
-  return <Button />;
+function Button({ name }) {
+  return <button type="button">{name}</button>;
 }
 
 export default Button;
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired
+};

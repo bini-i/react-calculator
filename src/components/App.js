386c3c525151d5c9a705bb45001bb4232a220847
bini-i <binyam.sh1@gmail.com>
2021-08-04 13:18:42 +0300

    Update App.js
    - created a class component with state for total, next and operation
    - created handleClick event handler and passed to ButtonPanel as props
    - state also passed to Display component as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import Display from "./Display";
 import "../App.css";
 import ButtonPanel from "./ButtonPanel";
 // eslint-disable-next-line no-unused-vars
 import calculate from "../logic/calculate";
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
-}
+// function App() {
+//   return (
+//     <>
+//       <Display />
+//       <ButtonPanel />
+//     </>
+//   );
+// }
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { totalState, nextState } = this.state;
+    const { total, next } = calculate(
+      { total: totalState, next: nextState },
+      buttonName
+    );
+    this.setState({
+      total,
+      next
+    });
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <>
+        <Display total={total} />
+        <ButtonPanel onClick={this.handleClick} />
+      </>
+    );
+  }
+}
 export default App;
